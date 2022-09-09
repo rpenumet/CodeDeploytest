2821c7e93fbf51338e418d538615c94ffa581975
rpenumet <86224290+rpenumet@users.noreply.github.com>
2022-09-09 18:51:55 +0530

    Update testing_py.sh

--- scripts/testing_py.sh
+++ scripts/testing_py.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/testing.py
+python3 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/dummy.py
